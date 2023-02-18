cdb89dc51f12c54aa6f088543ca69977566d7cc9
gikedo <gikedo2x@gmail.com>
2023-02-17 19:54:39 -0800

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (a = 0; a < 10; a++)
+		printf("%", a);
+	putchar('\n');
+	return (0);
+}
