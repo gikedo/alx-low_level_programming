d3c4dade9af1627658744182ef18c844c0eaf6fb
gikedo <gikedo2x@gmail.com>
2023-03-10 18:47:41 -0800

    3 mul.c fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,47 +1,18 @@
 #include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	if (argc != 3)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		printf("Error\n");
+		return (1);
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
