1ef0ab88d70d7e9a06960c17d13cd92214d762b5
gikedo <gikedo2x@gmail.com>
2023-03-20 14:32:17 -0700

    2-main fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - prints the name of the file it was compiled from
- *   *
- *    * Return: Everything worked ok
- *     */
-
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("%s\n", __BASE_FILE__);
-			return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
